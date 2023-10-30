f563350e4053d4f4d19588b3827574010c173932
BellaAdrien <Iadrien2020@fau.edu>
2023-10-30 10:24:43 -0400

    dummybox

---
+++ assests/js/script.js
@@ -0,0 +1,20 @@
+var aboutEl = document.querySelector("About");
+var workEl = document.querySelector("Work");
+
+
+
+
+
+
+
+
+
+
+function goHome() {
+    // nasaEL.classList.add("hide");
+    // favoriteEl.classList.add("hide");
+     workEl.classList.add("hide");
+    aboutEl.removeAttribute("hide");
+
+}
+
